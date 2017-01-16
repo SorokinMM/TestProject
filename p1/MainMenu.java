35bad6a004d05bc5d1536104c37e8ab534bf0a4e
Mihail Sorokin <4dddd@ukr.net>
2017-01-16 13:47:22 +0200

    commit 8

--- p1/MainMenu.java
+++ p1/MainMenu.java
@@ -4,5 +4,6 @@ public class MainMenu {
 	public static void main(String[] args){
 		Print.print("Hello world!!!");
 		Print.print("Hello world!!!");
+		Print.print("Hello world!!!");
 	}
 }
--- p1/Print.java
+++ p1/Print.java
@@ -3,5 +3,6 @@ package p1;
 public class Print {
 	public static void print(String text){
 		System.out.println(text);
+		System.out.println(text);
 	}
 }
