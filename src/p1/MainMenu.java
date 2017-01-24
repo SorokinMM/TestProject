66315ec3b0c6f80a504b572a597911d4781b15dc
Михаил Михайлович Сорокин <sorokin@vit.local>
2017-01-24 16:19:48 +0200

    commit 0.1

---
+++ src/p1/MainMenu.java
@@ -0,0 +1,7 @@
+package p1;
+
+public class MainMenu {
+	public static void main(String[] args){
+		Print.print("hello world");
+	}
+}
---
+++ src/p1/Print.java
@@ -0,0 +1,17 @@
+package p1;
+
+public class Print {
+	public static void print(String text){
+		System.out.println(text);
+	}
+	
+	public static void printISS53(){
+		System.out.println("ISS53");
+	}
+	
+	public static void printError(){
+		System.out.println();
+		System.out.println("ERROR");
+		System.out.println();
+	}
+}
